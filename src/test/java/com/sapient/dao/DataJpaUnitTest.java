474e86fc627a3e34fdb6d774f37189cd1b5600a5
SRINIVAS REDDY <srireddy4@publicissapient.com>
2020-03-03 00:34:08 +0530

    fifth commit

--- src/test/java/com/sapient/dao/DataJpaUnitTest.java
+++ src/test/java/com/sapient/dao/DataJpaUnitTest.java
@@ -2,14 +2,17 @@ package com.sapient.dao;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.internal.runners.TestClass;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SuiteClasses({DataJpaUnitTest.class})
 @DataJpaTest
 public class DataJpaUnitTest {
  
