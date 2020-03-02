19f984ce62cc36d605ce56e5f16fa2d819ba1425
SRINIVAS REDDY <srireddy4@publicissapient.com>
2020-03-03 00:55:57 +0530

    fifth commit

--- src/main/java/com/sapient/SpringbootRestJpaAppApplication.java
+++ src/main/java/com/sapient/SpringbootRestJpaAppApplication.java
@@ -1,8 +1,10 @@
 package com.sapient;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -16,6 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication(scanBasePackages="com.sapient")
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackages="com.sapient.dao")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @EntityScan("com.sapient.model")
 @EnableSwagger2
 public class SpringbootRestJpaAppApplication {
