fd095b193f01a71113632ed392bb2c8bf3848df6
SRINIVAS REDDY <srireddy4@publicissapient.com>
2020-03-02 23:43:37 +0530

    third commit

--- src/test/java/com/navis/springbootrestjpaapp/SpringbootRestJpaAppApplicationTests.java
+++ src/test/java/com/navis/springbootrestjpaapp/SpringbootRestJpaAppApplicationTests.java
@@ -3,7 +3,9 @@ package com.navis.springbootrestjpaapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.sapient.springbootrestjpaapp.SpringbootRestJpaAppApplication;
+
+@SpringBootTest(classes = SpringbootRestJpaAppApplication.class)
 class SpringbootRestJpaAppApplicationTests {
 
 	@Test
